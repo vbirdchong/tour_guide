e7b6558cf113bc016522c59af7182a2deff70ab3
chongchen <cc.chong.chen@gmail.com>
2017-09-10 13:19:31 +0800

    use switch case to implement the onClick callback

--- app/src/main/java/com/example/cccho/tourguide/MainActivity.java
+++ app/src/main/java/com/example/cccho/tourguide/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.cccho.tourguide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,39 +15,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView historicalSite = (TextView) findViewById(R.id.id_historical_site);
-        historicalSite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent historicalSiteIntent = new Intent(MainActivity.this, HistoricalsiteActivity.class);
-                startActivity(historicalSiteIntent);
-            }
-        });
+        historicalSite.setOnClickListener(this);
 
         TextView restaurants = (TextView) findViewById(R.id.id_restaurants);
-        restaurants.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent restaurantsIntent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                startActivity(restaurantsIntent);
-            }
-        });
+        restaurants.setOnClickListener(this);
 
         TextView events = (TextView) findViewById(R.id.id_event);
-        events.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent eventIntent = new Intent(MainActivity.this, EventActivity.class);
-                startActivity(eventIntent);
-            }
-        });
+        events.setOnClickListener(this);
 
         TextView traffic = (TextView) findViewById(R.id.id_traffic);
-        traffic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent trafficIntent = new Intent(MainActivity.this, TrafficActivity.class);
-                startActivity(trafficIntent);
-            }
-        });
+        traffic.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        Intent intent = null;
+        switch (id) {
+            case R.id.id_historical_site:
+                intent = new Intent(MainActivity.this, HistoricalsiteActivity.class);
+                break;
+            case R.id.id_event:
+                intent = new Intent(MainActivity.this, EventActivity.class);
+                break;
+            case R.id.id_traffic:
+                intent = new Intent(MainActivity.this, TrafficActivity.class);
+                break;
+            case R.id.id_restaurants:
+                intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                break;
+            default:
+                Log.e("MainActivity", "onClick id error: " + id);
+                break;
+        }
+
+        startActivity(intent);
     }
 }
